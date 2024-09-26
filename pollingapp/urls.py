ed12a50f14464b88b63b2c0dc86597931ee2fde7
leftidev <jes.bergstrom@gmail.com>
2024-09-26 10:02:28 +0300

    Updated app to use the generic Django pattern for views.

--- pollingapp/urls.py
+++ pollingapp/urls.py
@@ -2,6 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
+
+'''
+NOTE: This code does not use the generic Django pattern for views.
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
@@ -9,3 +19,4 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+'''
